3153b099ca0c64c40c87b6e50f48a934c54468bf
Aleksander Truszczyński <truszczynskia@student.mini.pw.edu.pl>
2021-05-21 21:20:21 +0200

    Summary

--- ato_tester.py
+++ ato_tester.py
@@ -8,6 +8,7 @@ class AtoTester:
     def __init__(self):
         self.tests = []
         self.test_records = []
+        self.summary_record = []
 
     def add_test(self):
         print("Dodawanie testu")
@@ -36,12 +37,25 @@ class AtoTester:
         self.test_records = []
 
         for t in self.tests:
+            game = t[0]
             reps = t[1]
+
+            p1_winrate = 0
+            av_game_len = 0
+
             for r in range(reps):
-                game = t[0]
                 game.play()
+                if game.winner == 1:
+                    p1_winrate += 1
+                av_game_len += len(game.log)
                 record = [game.p1.to_string(), game.p2.to_string(), game.alph_count, game.word_length, len(game.log), game.winner, ato_word_to_string(game.word, sep="|"), flatten_list_of_strings(game.log, separator="|")]
                 self.test_records.append(record)
+
+            p1_winrate = round(p1_winrate / reps)
+            p2_winrate = 1 - p1_winrate
+            av_game_len = round(av_game_len / reps, 2)
+
+            self.summary_record.append([game.p1.to_string(), game.p2.to_string(), game.alph_count, game.word_length, p1_winrate, p2_winrate, av_game_len])
 
     def print_results(self):
         for tr in self.test_records:
@@ -52,11 +66,20 @@ class AtoTester:
             os.mkdir(test_dir)
 
         now = datetime.datetime.now()
-        file = open(f"{test_dir}{os.path.sep}tests_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.csv", "w")
+
+        dir = f"{test_dir}{os.path.sep}"
+        suff = f"tests_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.csv"
+
+        res_file_name = f"{dir}{suff}"
+        summary_file_names = f"{dir}/summary_{suff}"
+        file = open(res_file_name, "w")
+        sum_file = open(summary_file_names, "w")
+
 
         history = True #TODO remove
         # TODO tego się chyba nie da wywołać z history = TRUE
         file.write("player_1,player_2,alph_count,word_len,game_len,winner")
+        sum_file.write("player_1,player_2,alph_count,word_len,p1_winrate,p2_winrate,av_game_len\n")
         if history:
             file.write(",final_word,log")
         file.write("\n")
@@ -65,3 +88,6 @@ class AtoTester:
             if history:
                 file.write(f",{tr[6]},{tr[7]}")
             file.write("\n")
+
+        for sr in self.summary_record:
+            sum_file.write(f"{sr[0]},{sr[1]},{sr[2]},{sr[3]},{sr[4]},{sr[5]},{sr[6]}\n")
